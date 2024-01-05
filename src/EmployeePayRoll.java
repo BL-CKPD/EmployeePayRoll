ab0c40c81551aa431dd51002fcffd6a57fbc3ee9
BL-CKPD <chandrakant.prasad@bridgelabz.com>
2024-01-05 13:44:03 +0530

    [Chandss] Add. Able to calculate daily wage of an employee

--- src/EmployeePayRoll.java
+++ src/EmployeePayRoll.java
@@ -5,12 +5,19 @@ public class EmployeePayRoll {
     public static void main(String[] args) {
 
         int IS_PRESENT = 1;
+        int RATE_PER_HOUR = 20;
+        int TOTAL_HOUR = 8;
+        
         double checker =  Math.floor(Math.random() * 10)%2;
 
+        int employeeDailyWage = RATE_PER_HOUR * TOTAL_HOUR;
+
         if(checker == IS_PRESENT){
-            System.out.println("Employee is present "+ checker);
+            System.out.println("Employee is present ");
+            System.out.println("Employee Daily Wage is : "+ employeeDailyWage);
         }else{
-            System.out.println("Employee is Absent "+ checker);
+            System.out.println("Employee is Absent ");
+            System.out.println("Employee Daily Wage is : "+ 0);
         }
     }
 }
